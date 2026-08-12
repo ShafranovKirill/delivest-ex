f35f081513faba33e60aba8eb3ce2b028f14786a
kiruxashafr <kirshafranov@gmail.com>
2026-08-12 15:14:02 +0300

    feat: branch schema

--- lib/delivest/net/branch.ex
+++ lib/delivest/net/branch.ex
@@ -1,2 +1,38 @@
 defmodule Delivest.Net.Branch do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Delivest.Identity.Staff
+
+  @type t :: %__MODULE__{}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @derive {
+    Flop.Schema,
+    filterable: [:name],
+    sortable: [:name, :inserted_at],
+    default_limit: 10,
+    default_order: %{
+      order_by: [:name],
+      order_directions: [:asc]
+    }
+  }
+
+  schema "branches" do
+    field :name, :string
+
+    has_many :staff, Staff
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(role, attrs) do
+    role
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 2, max: 50)
+    |> unique_constraint(:name, name: :branches__name__uk)
+  end
 end
