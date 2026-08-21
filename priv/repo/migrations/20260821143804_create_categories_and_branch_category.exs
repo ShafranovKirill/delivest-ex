3a82300912f61be3ab909d10bc7caf5ee79935e1
kiruxashafr <kirshafranov@gmail.com>
2026-08-21 19:38:57 +0300

    fix: db add category is_active

--- priv/repo/migrations/20260821143804_create_categories_and_branch_category.exs
+++ priv/repo/migrations/20260821143804_create_categories_and_branch_category.exs
@@ -5,6 +5,7 @@ defmodule Delivest.Repo.Migrations.CreateCategoriesAndBranchCategory do
     create table(:categories, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :name, :string, null: false
+      add :is_active, :boolean, default: false, null: false
 
       timestamps(type: :utc_datetime)
     end
