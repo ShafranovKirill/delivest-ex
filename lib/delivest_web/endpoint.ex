0a02e2f726f4ebac09e5d988ebb9c45448f59dda
ShafranovKirill <kirshafranov@gmail.com>
2026-09-02 18:01:06 +0300

    fix: cors and plugs

--- lib/delivest_web/endpoint.ex
+++ lib/delivest_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule DelivestWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :delivest
 
-  plug CORSPlug, origin: System.get_env("CLIENT_ORIGIN")
-
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
